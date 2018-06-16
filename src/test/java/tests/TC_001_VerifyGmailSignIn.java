aa7918ef69bbd5f62f59e6b56ca0440e0fb92f49
AT30 <39863373+AT30@users.noreply.github.com>
2018-06-16 15:42:07 +0530

    Update TC_001_VerifyGmailSignIn.java

--- src/test/java/tests/TC_001_VerifyGmailSignIn.java
+++ src/test/java/tests/TC_001_VerifyGmailSignIn.java
@@ -2,6 +2,11 @@ package tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import atu.testng.reports.ATUReports;
+import atu.testng.reports.logging.LogAs;
+import atu.testng.selenium.reports.CaptureScreen;
+import atu.testng.selenium.reports.CaptureScreen.ScreenshotOf;
 import pageClasses.SignInPage;
 import utilities.ParentTest;
 
@@ -18,10 +23,12 @@ public class TC_001_VerifyGmailSignIn extends ParentTest {
 			Assert.assertTrue(status == true,"User is not logged in");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			Assert.fail(e.getMessage());
+			ATUReports.add("Failed", e.getMessage(), "", LogAs.FAILED, new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+//			Assert.fail(e.getMessage());
 		} catch (AssertionError e) {
 			System.out.println(e.getMessage());
-			Assert.fail(e.getMessage());
+			ATUReports.add("Failed", e.getMessage(), "", LogAs.FAILED, new CaptureScreen(ScreenshotOf.BROWSER_PAGE));
+//			Assert.fail(e.getMessage());
 		}
 	}
 }
